9104e8d5c739ef89813b966ab6335de749b1516e
Yaroslav <ufamoonguard@gmail.com>
2018-05-13 13:55:22 +0500

    Added indestructible block

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,20 +1,46 @@
 ﻿using UnityEngine;
 
-public class Block : MonoBehaviour {
-
-    public int MaxHit;
-    public bool IsDestroy = false;
+public class Block : MonoBehaviour
+{
+    //public bool IsDestroy = false;
+    public Sprite[] HitSprites;
 
     [SerializeField]
     private int timesHit;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        bool isBreakable = this.CompareTag("Breakable");
+        if(isBreakable)
+        {
+            HandleHits();
+        }
+    }
+
+    private void HandleHits()
+    {
+        var maxHit = HitSprites.Length + 1;
         timesHit++;
-        if(timesHit >= MaxHit)
+        if (timesHit >= maxHit)
         {
             Destroy(gameObject);
         }
+        else
+        {
+            LoadSprite();
+        }
+    }
+
+    private void LoadSprite()
+    {
+        var spriteIndex = timesHit - 1;
+        spriteRenderer.sprite = HitSprites[spriteIndex];
     }
 
 }
